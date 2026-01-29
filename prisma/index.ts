dc6bb4a669a14dedd955939fdb13814b84e35a25
root <root@ser458984887747.local>
2026-01-30 00:40:50 +0800

    fix: 修复 prisma 客户端创建问题

--- prisma/index.ts
+++ prisma/index.ts
@@ -2,9 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  adapter: process.env.KUN_DATABASE_URL,
-})
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
