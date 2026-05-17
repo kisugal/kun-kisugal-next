fedbebb19e37a2705673e1e78f91cd5ccbd98a07
kisugal <1379227919@qq.com>
2026-05-18 01:16:54 +0800

    Update page.tsx

--- app/(main)/topic/search/page.tsx
+++ app/(main)/topic/search/page.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
 	title: `话题搜索 - ${kunMoyuMoe.title}`,
 	description: "搜索话题讨论",
 };
+
+export const dynamic = 'force-dynamic'
+
 export default async function TopicSearchPage() {
 	return (
 		<TopicSearchListPage />
