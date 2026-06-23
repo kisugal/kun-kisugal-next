55f97517e3ef313e5076d62c8bb718d722c0a066
kisugal <1379227919@qq.com>
2026-06-23 22:45:57 +0800

    feat: 添加搜索组件 SearchGlgc

---
+++ components/search/glgc.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export const SearchGlgc = () => {
+  return (
+    <div>
+      <Link
+        href="https://afengy.app/?ref_id=006f5ccb-b0d3-471b-a674-de5e5114ed67"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          alt=""
+          width={1200}
+          height={200}
+          src="https://r2.sakinori.top/%E9%A3%8E%E6%9C%88AI/1200x200-03.gif"
+        />
+      </Link>
+    </div>
+  )
+}
