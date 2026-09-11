132fea36a2bb79a9f9d1447ebac1904e18aeccd2
kisugal <1379227919@qq.com>
2026-09-11 11:06:04 +0800

    fix:更新广告链接

--- components/kun/top-bar/MobileSidebar.tsx
+++ components/kun/top-bar/MobileSidebar.tsx
@@ -111,7 +111,7 @@ const navSections = [
         name: '翻墙Vpn推荐',
         description:
           '翻墙Vpn推荐，加速下载！觉得下载资源慢？觉得加载页面不丝滑？',
-        href: 'https://www.tspeedcat.top/#/register?code=FttmLPkV',
+        href: 'https://tmsretv.top/#/register?code=MWh9TU70',
         icon: HeartIcon
       }
     ]
--- components/topic/TopicListClient.tsx
+++ components/topic/TopicListClient.tsx
@@ -88,7 +88,7 @@ const glgc = [
     title: '翻墙Vpn推荐',
     imageurl:
       'https://d.acgll.com/%E5%9B%BE%E7%89%87%E5%AD%98%E5%82%A8/%E5%B9%BF%E5%91%8A%E5%9B%BE1180x720.jpg',
-    url: 'https://www.tspeedcat.top/#/register?code=FttmLPkV',
+    url: 'https://tmsretv.top/#/register?code=MWh9TU70',
     content: '翻墙Vpn推荐，加速下载！觉得下载资源慢？觉得加载页面不丝滑？'
   },
   {
